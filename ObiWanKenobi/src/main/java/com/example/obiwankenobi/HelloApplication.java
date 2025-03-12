e6992267a95c1d8f9bd7a297ae06f09e3d7dde03
Florek <bf125115@stud.ur.edu.pl>
2025-03-12 23:43:29 +0100

    jakies widoki

--- ObiWanKenobi/src/main/java/com/example/obiwankenobi/HelloApplication.java
+++ ObiWanKenobi/src/main/java/com/example/obiwankenobi/HelloApplication.java
@@ -4,15 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/com/example/obiwankenobi/views/pane.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("ObiWanKenobi!");
         stage.setScene(scene);
         stage.show();
     }
--- ObiWanKenobi/src/main/java/module-info.java
+++ ObiWanKenobi/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module com.example.obiwankenobi {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
 
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires jakarta.persistence;
+    requires AnimateFX;
 
     opens com.example.obiwankenobi to javafx.fxml;
     exports com.example.obiwankenobi;
